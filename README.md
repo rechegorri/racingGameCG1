1571bec9b4379e81d4f3c0c0d81d2534694faba5
REchegorri <rodrigo_echegorri@yahoo.com.br>
2018-09-27 22:05:54 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# racingGameCG1
---
+++ src/cg1_20180816/CarroNPC.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cg1_20180816;
+
+/**
+ *
+ * @author Livnick
+ */
+public class CarroNPC extends Base{
+    public CarroNPC(String url){
+        super(url);
+        this.isPolice = false;
+    }
+}
